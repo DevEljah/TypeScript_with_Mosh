8025a1e17591b05f24f0fee6422c57521a32419e
Jahid Eljah <jahideljah@gmail.com>
2023-01-24 19:15:46 +0100

    first commit

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let age = 24;
+age = 20;
+if (age < 50)
+    age += 10;
+console.log(age);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+let age: number = 24;
+age = 20;
+
+if (age < 50) age += 10;
+console.log(age);
