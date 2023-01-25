1f29be80c3a769d03351ff1673d01c6ccd613dce
Jahid Eljah <jahideljah@gmail.com>
2023-01-25 13:46:14 +0100

    more steps :)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,32 @@
-let age: number = 24;
+/* let age: number = 24;
 age = 20;
 
 if (age < 50) age += 10;
 console.log(age);
+ */
+
+//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+let sales = 123_456_789; // <== let sales: number = 123_456_789;
+let course = "TypeScript"; // <== let course: string = "TypeScript";
+let is_puplished = true; // <== let is_puplished: boolean = true;
+let level; // TS assumes it is type is "any"
+// ==>
+/* level = 1;
+level = "a"; // don't use is like this! */
+
+//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+/* function render(doc) {
+    console.log(doc);  
+} */
+// ==>
+/* function render(doc: any) {
+  console.log(doc);
+} */
+// ==> or // "noImplicitAny": true, // turn it to false in tsconfig.json
+
+//////////////////////////////////////////////////////
+/////////////////////      Arrays      ///////////////
+//////////////////////////////////////////////////////
+
